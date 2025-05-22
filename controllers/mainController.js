8443992d97c19e512387490d0bbaecdf2fcee6c9
Luis Fernando Arevalo <luisferarevalou@gmail.com>
2025-05-22 14:35:09 -0500

    agenda casi lista

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,11 @@
 const controller = {}
 import sheetdb from 'sheetdb-node';
 
-controller.inicio = (req, res) => {
+controller.inicio = (req,res) => {
+  res.render('inicio')
+}
+
+controller.agenda = (req, res) => {
 
   const config = {
     address: 'job0hpcx2z0oc',
@@ -20,10 +24,11 @@ client.read({ limit: 20, offset: 0, sheet: 'showAgenda' }).then(function(data) {
 
   for (let i = 1; i < servicio.length; ++i) {
     
-    console.log(servicio[i])
-    agenda.push(servicio[i])
-  }
+    const elementos = servicio[i].replace(/"/g, '')
+    agenda.push(elementos)
 
+
+  }
 
   res.render('main', {
     agenda
@@ -33,5 +38,6 @@ client.read({ limit: 20, offset: 0, sheet: 'showAgenda' }).then(function(data) {
 });
 
 }
+
 
 export { controller }
--- routes/main.js
+++ routes/main.js
@@ -5,6 +5,8 @@ import { controller } from '../controllers/mainController.js';
 const app = express()
 
 router.get('/', controller.inicio)
+router.get('/agenda', controller.agenda)
+
 
 
 export { router }
